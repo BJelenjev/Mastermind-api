130a065d5814ccbb1500857d820e7b31a7414717
BJelenjev <boacon1@yahoo.com>
2017-07-31 17:29:15 +0200

    creation of games model

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -1,12 +1,34 @@
 // games-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
+
+  const codeSchema = new Schema({
+    initcode: { type: [Number], required: true},
+    won: { type: Boolean, default: false},
+  });
+
+  const playersSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'users' },
+  });
+
+  const guessesSchema = new Schema({
+    guess: { type: [Number], required: true},
+    clue: { type: [Number], required: true},
+  });
+
   const games = new Schema({
-    text: { type: String, required: true },
+    code: [codeSchema],
+    players: [playersSchema],
+    guesses: [guessesSchema],
+    turn: { type: Number, default: 0 },
+    started: { type: Boolean, default: false },
+    winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
+    ownerId: { type: Schema.Types.ObjectId, ref: 'users' },
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
