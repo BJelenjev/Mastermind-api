0049d98b28aa5f915c0b18173f4d11f314ec513e
BJelenjev <boacon1@yahoo.com>
2017-07-31 16:55:05 +0200

    User model ammended

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,12 +1,15 @@
 // users-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
   const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
-  const users = new Schema({
-    text: { type: String, required: true },
+  const users = new mongooseClient.Schema({
+    name: { type: String, required: true },
+    email: {type: String, unique: true},
+    password: { type: String },
+
+
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
