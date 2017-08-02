d37522e537ec5e092d9d69adb32efbd0d43ebcbd
BJelenjev <boacon1@yahoo.com>
2017-08-02 11:45:49 +0200

    moved errors up

--- src/hooks/check-guess.js
+++ src/hooks/check-guess.js
@@ -6,8 +6,9 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     // Hooks can either return nothing or a promise
     // that resolves with the `hook` object for asynchronous operations
 
-    //see if hook has data this should be an 4 length array
+    //see if hook has data guess: and that the array doesn't contain 0
     if (hook.data.guess === undefined) return Promise.resolve(hook);
+    if (hook.data.guess.includes(0)) return Promise.resolve(hook);
     //check if user is one of the players
     const { user } = hook.params;
     return hook.app.service('games').get(hook.id)
@@ -19,6 +20,18 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
         const hasTurn = playerIds.indexOf(user._id)  === turnState;
         const guessValid = hook.data.guess.length === code.length;
         const turnLoss = hook.data.turn > 10 ;
+
+        if (!joined) {
+          throw new errors.Unprocessable('You are not a player in this game, so you can not play!');
+        }
+
+        if (!hasTurn) {
+          throw new errors.Unprocessable('It is not your turn to guess!');
+        }
+
+        if (!guessValid) {
+          throw new errors.Unprocessable('Your guess is not complete!');
+        }
 
         function spotOnGuess(guess,code){
           var i = guess.length;
@@ -41,17 +54,6 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
         }
 
 
-        if (!joined) {
-          throw new errors.Unprocessable('You are not a player in this game, so you can not play!');
-        }
-
-        if (!hasTurn) {
-          throw new errors.Unprocessable('It is not your turn to guess!');
-        }
-
-        if (!guessValid) {
-          throw new errors.Unprocessable('Your guess is not complete!');
-        }
 
 
         //take the hook data and generate its clue
