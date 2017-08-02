87894ec11816d641003285f3fe98bbedf9656431
BJelenjev <boacon1@yahoo.com>
2017-08-02 15:15:26 +0200

    create-game test

--- test/hooks/create-game.test.js
+++ test/hooks/create-game.test.js
@@ -1,17 +1,42 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const createGame = require('../../src/hooks/create-game');
 
 describe('\'createGame\' hook', () => {
-  it('runs the hook', () => {
-    // A mock hook object
-    const mock = {};
-    // Initialize our hook with no options
-    const hook = createGame();
+  // A mock hook object
+  const mock = {
+    params: {
+      user: { _id: 'abc123' }
+    },
+    data: {
 
+    }
+  };
+  // Initialize our hook with no options
+  const hook = createGame();
+
+  it('assigns the current user', () => {
     // Run the hook function (which returns a promise)
     // and compare the resulting hook object
     return hook(mock).then(result => {
-      assert.equal(result, mock, 'Returns the expected hook object');
+      expect(result.data.userId).to.eq(mock.params.user._id);
     });
   });
+
+  it('creates the code of 4 positions', () => {
+    // Run the hook function (which returns a promise)
+    // and compare the resulting hook object
+    return hook(mock).then(result => {
+      expect(result.data.code.length).to.eq(4);
+    });
+  });
+
+  it('adds the current user as the first player', () => {
+    // Run the hook function (which returns a promise)
+    // and compare the resulting hook object
+    return hook(mock).then(result => {
+      expect(result.data.players.length).to.eq(1);
+      expect(result.data.players[0].userId).to.eq('abc123');
+    });
+  });
+
 });
