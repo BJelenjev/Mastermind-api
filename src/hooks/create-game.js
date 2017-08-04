d2b066366ab7c66eb32c6d8d09948bcbcc639ceb
BJelenjev <boacon1@yahoo.com>
2017-08-04 05:15:19 +0200

    added semi collons to create-game

--- src/hooks/create-game.js
+++ src/hooks/create-game.js
@@ -13,7 +13,7 @@ function select(array) {
 }
 
 function seedColors() {
-  return ["gray"].concat("red orange yellow green blue indigo".split(" "))
+  return ['gray'].concat('red orange yellow green blue indigo'.split(' '));
 }
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
@@ -22,20 +22,20 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     const { user } = hook.params;
 
     // assign the owner of the game
-    hook.data.ownerId = user._id
+    hook.data.ownerId = user._id;
 
     // add the owner to the players, as the first player in the game
-    hook.data.players = [{_id: user._id}]
+    hook.data.players = [{_id: user._id}];
 
     // create a code to guess
     const initcode = select('123456'.split(''));
     hook.data.code = initcode.map((initcode) => Number(initcode));
-    
+
     // and seed colors (add a color scheme generator later).
     // The first color (index 0) should be "gray", since our initcode values only
     // go 1-6 we need to have 7 colors exactly
-    hook.data.colors = seedColors()
-    
+    hook.data.colors = seedColors();
+
     return Promise.resolve(hook);
   };
 };
