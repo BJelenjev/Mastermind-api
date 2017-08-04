1fe0409d1d5921ded1f0d380dce4ac1031531b2b
Julik Tarkhanov <me@julik.nl>
2017-08-04 16:25:03 +0200

    _Really_ make sure code[] is properly concealed

--- src/hooks/conceal-code.js
+++ src/hooks/conceal-code.js
@@ -1,5 +1,8 @@
 // Recursively remove the "code" property from the given object or Array of objects
 const concealGameCode = (data) => {
+  if(data.data) { // If this is a paginated response, where the collection is enveloped in data
+    return Object.assign({}, data, {data: data.data.map(concealGameCode)})
+  }
   if(data.map) { // If the data is an array
     return data.map(concealGameCode);
   } else {
