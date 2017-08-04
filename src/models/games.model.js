87e7cdc4eef695ee3ebf1870a3c32226eb0f787b
Julik Tarkhanov <me@julik.nl>
2017-08-04 13:35:21 +0200

    Simplify game state transitions

--- src/models/games.model.js
+++ src/models/games.model.js
@@ -16,17 +16,17 @@ module.exports = function (app) {
     numApprox:   {type: Number, required: true},
   });
 
+  const GAME_STATES = ['inProgress', 'computerWon', 'playerWon']
+  
   const games = new Schema({
     code: { type: [Number], required: true},
     colors: { type: [String], required: true}, // CSS-compatible color names or hex colors prefixed with '#'
     players: [playerSchema],
     guesses: [guessSchema],
     turn: { type: Number, default: 0 },
-    started: { type: Boolean, default: false },
     winnerId: { type: Schema.Types.ObjectId, ref: 'users' },
     ownerId: { type: Schema.Types.ObjectId, ref: 'users' },
-    won: { type: Boolean, default: false},
-    loss: { type: Boolean, default: false},
+    gamePhase: { type: String, enum: GAME_STATES , default: GAME_STATES[0] },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
